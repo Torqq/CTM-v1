6c11918d348dffd4d7e228314b37581180eb7d66
Bilâl Adanur <88437058+Torqq@users.noreply.github.com>
2023-07-04 21:08:53 +0200

    Delete core.cs

--- CTM v1/Form1.cs
+++ CTM v1/Form1.cs
@@ -1,7 +1,17 @@
+using System.Diagnostics.Eventing.Reader;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolTip;
+using System.Runtime.CompilerServices;
+
 namespace CTM_v1
 {
     public partial class CTM : Form
     {
+        TextBox? explication;
+        TextBox[]? textboxesHorraire;
+        TextBox[]? textboxesTemperature;
+        TextBox[]? indiqHorraire;
+        TextBox[]? indiqTemperature;
+
         public CTM()
         {
             InitializeComponent();
@@ -20,10 +30,63 @@ namespace CTM_v1
             plusieursProgValiderNbProg.Visible = plusieursProgrammes.Checked;
         }
 
-        private void buttonReinitialiser_Click(object sender, EventArgs e)
+        private void buttonReinitialiser_Click(object sender, EventArgs e) 
         {
             unProgramme.Checked = false;
             plusieursProgrammes.Checked = false;
+            unProgPlageHorraires.Text = "";
+            unProgPlageHorraires.ReadOnly = false;
+            unProgPlageHorraires.Text = "";        
+
+            if(explication != null)
+            {
+                explication.Visible = false;
+            }
+
+            if (textboxesHorraire != null)
+            {
+                foreach (TextBox textBox in textboxesHorraire)
+                {
+                    if (textBox != null)
+                    {
+                        textBox.Visible = false;
+                    }
+                }
+            }
+
+            if (textboxesTemperature != null)
+            {
+                foreach (TextBox textBox in textboxesTemperature)
+                {
+                    if (textBox != null)
+                    {
+                        textBox.Visible = false;
+                    }
+                }
+            }
+
+            if (indiqHorraire != null)
+            {
+                foreach (TextBox textBox in indiqHorraire)
+                {
+                    if (textBox != null)
+                    {
+                        textBox.Visible = false;
+                    }
+                }
+            }
+
+            if (indiqTemperature != null)
+            {
+                foreach (TextBox textBox in indiqTemperature)
+                {
+                    if (textBox != null)
+                    {
+                        textBox.Visible = false;
+                    }
+                }
+            }
+
         }
 
         private void objectifDuProgramme_TextChanged(object sender, EventArgs e)
@@ -68,15 +131,44 @@ namespace CTM_v1
 
         }
 
-        private void unProgPlageHorraires_TextChanged(object sender, EventArgs e)
+        public void unProgPlageHorraires_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void unProgValiderPlageHorraire_Click(object sender, EventArgs e)
+        public int valeurPlageHorraire { get; private set; }
+
+        public void unProgValiderPlageHorraire_Click(object sender, EventArgs e)
         {
-            string valeurInscrit = unProgPlageHorraires.Text;
-            int valeurPlageHorraire = int.Parse(valeurInscrit);
+            if (unProgPlageHorraires.Text != "")
+            {
+                string valeurInscrit = unProgPlageHorraires.Text;
+                int tempValeurPlageHorraire;
+
+                if (int.TryParse(valeurInscrit, out tempValeurPlageHorraire) && tempValeurPlageHorraire != 0)
+                {
+                    if(tempValeurPlageHorraire < 24)
+                    {
+                        valeurPlageHorraire = tempValeurPlageHorraire;
+                        unProgPlageHorraires.ReadOnly = true;
+
+                        // Appel la méthode de la classe 'core'
+                        UnProgExplication();
+                        UnProgBoxCreation(valeurPlageHorraire);
+                    }
+                    else 
+                    {
+                        MessageBox.Show("Il n'est pas possible d'avoir plus de plages horraires qu'il n'y à d'heure dans la journée.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
+                else
+                {
+                    MessageBox.Show("Veuillez entrer un nombre valide.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                
+            }
+
         }
 
         private void plusieursProgrammes_CheckedChanged(object sender, EventArgs e)
@@ -111,6 +203,79 @@ namespace CTM_v1
         {
             string valeurInscrit = nombreProgParSemaine.Text;
             int valeurNbProgParSemaine = int.Parse(valeurInscrit);
+        }
+
+        private void UnProgExplication()
+        {
+            int x = 12;
+            int y = 350;
+            explication = new TextBox();
+            explication.Location = new Point(x, y);
+            explication.Size = new Size(474, 44);
+            explication.BackColor = Color.CornflowerBlue;
+            explication.Multiline = true;
+            explication.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            explication.Text = "Veuillez remplir les données dans les cases indiqués ci-dessous dans l'ordre tespectif.";
+            Controls.Add(explication);
+        }
+
+        public void UnProgBoxCreation(int valeurPlageHorraire)
+        {           
+            int nbPlageHorraire = valeurPlageHorraire;
+            int x = 12;
+            int y = 350;
+            int v = 0;
+            const int largeur = 100;
+            const int hauteur = 25;
+
+            IList<int> valeurTemperature = new List<int>();
+            IList<int> valeurHorraire = new List<int>();
+
+            textboxesHorraire = new TextBox[nbPlageHorraire];
+            textboxesTemperature = new TextBox[nbPlageHorraire];
+            indiqHorraire = new TextBox[nbPlageHorraire];
+            indiqTemperature = new TextBox[nbPlageHorraire];
+
+            for (int i = 0; i < nbPlageHorraire; i++)
+            {
+                v++;
+                textboxesHorraire[i] = new TextBox();
+                textboxesHorraire[i].Location = new Point(x, y + 50 + i * 50);
+                textboxesHorraire[i].Size = new Size(largeur, hauteur);
+                Controls.Add(textboxesHorraire[i]);
+
+                indiqHorraire[i] = new TextBox();
+                indiqHorraire[i].Location = new Point(x + 100, y + 50 + i * 50);
+                indiqHorraire[i].Size = new Size(largeur, hauteur);
+                indiqHorraire[i].Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+                indiqHorraire[i].BackColor = SystemColors.InactiveCaption;
+                indiqHorraire[i].BorderStyle = BorderStyle.None;
+                indiqHorraire[i].Text = ("<-Horraires " + v);
+                indiqHorraire[i].Enabled = false;
+                indiqHorraire[i].ReadOnly = true;
+                Controls.Add(indiqHorraire[i]);
+            }
+
+            v = 0;
+            for (int i = 0; i < nbPlageHorraire; i++)
+            {
+                v++;
+                textboxesTemperature[i] = new TextBox();
+                textboxesTemperature[i].Location = new Point(x + 373, y + 50 + i * 50);
+                textboxesTemperature[i].Size = new Size(largeur, hauteur);
+                Controls.Add(textboxesTemperature[i]);
+
+                indiqTemperature[i] = new TextBox();
+                indiqTemperature[i].Location = new Point(x + 273, y + 50 + i * 50);
+                indiqTemperature[i].Size = new Size(largeur, hauteur);
+                indiqTemperature[i].Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+                indiqTemperature[i].BackColor = SystemColors.InactiveCaption;
+                indiqTemperature[i].BorderStyle = BorderStyle.None;
+                indiqTemperature[i].Text = ("Temperature " + v + "->");
+                indiqTemperature[i].Enabled = false;
+                indiqTemperature[i].ReadOnly = true;
+                Controls.Add(indiqTemperature[i]);
+            }
         }
     }
 }
